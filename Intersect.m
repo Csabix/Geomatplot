4bf5daedfe8c5392eaf47dffa87cd98124ba9fa5
Csaba Bálint <csabix.balint@gmail.com>
2023-07-19 16:03:28 +0200

    Added help to Intersect
    it is also an implementation plan

--- Intersect.m
+++ Intersect.m
@@ -1,4 +1,35 @@
 function h = Intersect(varargin)
+% INTERSECT  intersects two Geomatplot curves
+%   INTERSECT({a,b})  intersects curve a and b producing a single intersection point.
+%       If there are more intersection points, only the first one found will be shown.
+%       If no intersections occur, the intersection point will still exist but will be
+%       in an undefined state. Inputs a and b may be labels of existing curves. The
+%       intersection point is assigned a label automatically.
+%
+%   INTERSECT(label,{a,b})  provides a label for the intersection point.
+%
+%   INTERSECT(N,{a,b})  creates N number of intersection points between curves a and b.
+%       If there M < N intersections, then the last N-M number of intersections will be
+%       undefined. Label assignement is automatic.
+%
+%   INTERSECT({label1,label2,...},{a,b})  assignes labels to multiple intersections.
+%       The number of given labels determine the number of intersection points created.
+%
+%   INTERSECT(parent,___)  draws onto the given geomatplot, axes, or figure instead of
+%       the current one.
+%
+%   INTERSECT(___,color)  specifies the color of the intersection, the default is 'k'.
+%
+%   INTERSECT(___,Name,Value)  specifies additional properties using one or more Name,
+%       Value pairs arguments.
+%
+%   h = INTERSECT(___)  returns the intersection points as an array of handles.
+%
+%   [h,g] = INTERSECT(___)  also returns the intersection point sequence object. This
+%       allows more dynamic intersection number handling.
+%
+%   See also GEOMATPLOT
+
 
     [parent,label,labels,args] = parseinputs(varargin{:});
     function v = intersect(a,b)
@@ -19,6 +50,7 @@ function [parent,label,labels,args] = parseinputs(varargin) % todo
     p.addOptional('Parent'     , [], @(x) isa(x,'Geomatplot') || isa(x,'matlab.graphics.axis.Axes') || isa(x,'matlab.ui.Figure'));
     p.addOptional('Label'      , [], @(x) isvarname(x));
     p.addOptional('Labels'     , [], @drawing.isLabelList);
+    %o.addOptional('Max')
     p.addOptional('Color'      ,'k', @drawing.isColorName);
 
     p.KeepUnmatched = true;

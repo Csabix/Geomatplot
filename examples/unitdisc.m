e03c5c64e26984e45c33a686c02f339824f867ed
Csaba Bálint <csabix.balint@gmail.com>
2024-01-28 01:20:06 +0100

    Fix Point 'c'/'m' hack and 'Visible' option for PointSequence

--- examples/unitdisc.m
+++ examples/unitdisc.m
@@ -1,33 +1,37 @@
-%% Unit Disc
 clf; disp 'Unit Disc'
 
-orig = Point('orig',@() [0 0],'LabelV','off');
-one1 = Scalar('one1',@() 1);
-unitcircle = Circle(orig,one1,'m');
+o = Point('o',@()[0 0],'m',3,'LabelV','off');
+Circle(o,Scalar(@()1),'m'); % unit circle
+b = Point('b', [.5 0],'r');
 
-n = 5;
-rng(0); ps=cell(n,1); is=ps;
+n = 4; rng(0);
+ps=cell(n,1); is=ps;
 for i = 1:n
     t = rand*2*pi; r = .5*rand+1.1;
     ps{i} = Point([cos(t) sin(t)]*r);
-    is{i} = Mirror(ps{i}.label+"_", ps{i}, unitcircle,'c'); % 1/conj(a)
+    is{i} = Point(ps{i}.label+"_",ps{i},@(a) a./(a.*a*[1;1]),'c'); % 1/conj(a)
 end
 
-b = Point('b', [.5 0],'r');
-ps = PointSequence(is{:});
+% Pseudo-hyperbolic Voronoi diagram
+Image(PointSequence(is{:},'Vis','off'),@hypervoronoi,[-1.5 -1.5],[1.5 1.5]);
+xlim([-2 2]); ylim([-1.5 1.5])
 
-Image(ps,@hipvoronoi,[-1 -1],[1 1]);
-xlim([-1.5 1.5]); ylim([-1.5 1.5])
+%% Voronoi diagram
+DT = CustomValue(PointSequence(ps{:}),@delaunayTriangulation);
+SegmentSequence(DT,@drawVoronoi,0,'m');
 
 %%
-function inds = hipvoronoi(z,pts) % z is a N*M matrix
+function inds = hypervoronoi(z,pts) % z is a N*M matrix
     dist = inf(size(z));
     inds = z*0;
     for i=1:length(pts)
-        tmp = abs(z-pts(i));
-        mask = tmp<dist;
-        dist(mask) = tmp(mask);
-        inds(mask) = i;
+        tmp = abs(  (z-pts(i)) ./ (1-z.*conj(pts(i)))  );
+        inds(tmp<dist) = i;
+        dist = min(tmp,dist);
     end
 end
-
+function xy = drawVoronoi(dt)
+    [C,r] = voronoiDiagram(dt); % kinda stupid but returns inf for all unbounded region vertices
+    r = cellfun(@(x) [x 1],r,'UniformOutput',false);
+    xy = C(horzcat(r{:}),:);
+end

9b0eceff2be9c917065cae7dd8152efd59642d29
Levente Fodor <leventefodor@gmail.com>
2024-04-12 15:41:59 +0200

    Added point and line size changing.

--- gui/PropertiesPanel.m
+++ gui/PropertiesPanel.m
@@ -3,6 +3,7 @@ classdef PropertiesPanel < handle
     properties (Access = private)
         propertiesPanel
         labelPanel
+        markerPanel
         geometry
     end
 
@@ -18,6 +19,7 @@ classdef PropertiesPanel < handle
         function delete(o)
             delete(o.propertiesPanel);
             delete(o.labelPanel);
+            delete(o.markerPanel);
         end
     end
 
@@ -25,10 +27,10 @@ classdef PropertiesPanel < handle
         function o = createPointPanel(o,fig,pos,geometry)
             o.geometry = geometry;
             propPanel = uipanel(fig);
-            propPanel.Position = [pos + [10 -50], 80, 35];
+            propPanel.Position = [pos + [10 -50], 105, 35];
             o.propertiesPanel = propPanel;
 
-            grid = PropertiesPanel.createContainerGrid(propPanel,{{25},{25 25}},[5 5 5 5]);
+            grid = PropertiesPanel.createContainerGrid(propPanel,{{25},{25 25 25}},[5 5 5 5]);
             addlistener (grid, 'ButtonDown', @(~,~) PropertiesPanel.closeSubPanels(o));
             PropertiesPanel.createColorDropdown(o,grid,[1 1]);
     
@@ -38,16 +40,25 @@ classdef PropertiesPanel < handle
             labelButton.Tooltip = "Label";
             labelButton.Text = "";
             labelButton.Icon = imread("gui\resources\Label.png");
-            labelButton.ButtonPushedFcn = @(src,evt) PropertiesPanel.switchLabelPanel(o,src,evt);
+            labelButton.ButtonPushedFcn = @(~,~) PropertiesPanel.switchLabelPanel(o);
+
+            size = geometry.fig.MarkerSize - 2;
+            markerButton = uibutton(grid);
+            markerButton.Layout.Row = 1;
+            markerButton.Layout.Column = 3;
+            markerButton.Tooltip = "Marker";
+            markerButton.Text = "";
+            markerButton.Icon = imread("gui\resources\Marker.png");
+            markerButton.ButtonPushedFcn = @(~,~) PropertiesPanel.switchMarkerPanel(o,size,[1 10]);
         end
 
         function o = createLinePanel(o,fig,pos,geometry)
             o.geometry = geometry;
             propPanel = uipanel(fig);
-            propPanel.Position = [pos + [10 -50], 70, 35];
+            propPanel.Position = [pos + [10 -50], 105, 35];
             o.propertiesPanel = propPanel;
 
-            grid = PropertiesPanel.createContainerGrid(propPanel,{{25}, {25 25}},[5 5 5 5]);
+            grid = PropertiesPanel.createContainerGrid(propPanel,{{25}, {25 25 25}},[5 5 5 5]);
             PropertiesPanel.createColorDropdown(o,grid,[1 1]);  
 
             styles = {'-','--',':','-.'};
@@ -63,11 +74,21 @@ classdef PropertiesPanel < handle
             styleDropdown.Items = repmat({''}, numel(styles), 1);
             styleDropdown.ItemsData = styles;
             styleDropdown.Tooltip = "Linestyle";
+            addlistener (styleDropdown, 'DropDownOpening', @(~,~) PropertiesPanel.closeSubPanels(o));
             for i = 1:numel(styles)
                 style = uistyle('Icon',icons{i});
                 addStyle(styleDropdown,style,"item",i);
             end
             styleDropdown.Value = geometry.fig.LineStyle;
+
+            size = geometry.fig.LineWidth - 1;
+            markerButton = uibutton(grid);
+            markerButton.Layout.Row = 1;
+            markerButton.Layout.Column = 3;
+            markerButton.Tooltip = "Marker";
+            markerButton.Text = "";
+            markerButton.Icon = imread(icons{1});
+            markerButton.ButtonPushedFcn = @(~,~) PropertiesPanel.switchMarkerPanel(o,size,[1 5]);
         end
     end
 
@@ -122,11 +143,51 @@ classdef PropertiesPanel < handle
             showCheckbox.ValueChangedFcn = @(src,evt) PropertiesPanel.switchLabel(o,src,evt);
         end
 
+        function createMarkerPanel(o,size,limits)
+            markerPanel = uipanel(o.propertiesPanel.Parent);
+            markerPanel.Position = [o.propertiesPanel.Position(1:2) + [75 -40], 105, 35];
+            o.markerPanel = markerPanel;
+
+            grid = PropertiesPanel.createContainerGrid(markerPanel,{{25}, {70, 25}},[5 5 5 5]);
+            
+            markerSize = uislider(grid);
+            markerSize.Layout.Row = 1;
+            markerSize.Layout.Column = 1;
+            markerSize.Limits = limits;
+            markerSize.MajorTicks = [];
+            markerSize.MinorTicks = [];
+            markerSize.Value = size;
+            markerSize.ValueChangingFcn = @(src,evt) PropertiesPanel.setMarkerSize(o,src,evt);
+
+            markerSizeLabel = uilabel(grid);
+            markerSizeLabel.Layout.Row = 1;
+            markerSizeLabel.Layout.Column = 2;
+            markerSizeLabel.Text = string(size);
+        end
+
         function grid = createContainerGrid(panel,size,padding)
             grid = uigridlayout(panel);
             grid.RowHeight = size{1};
             grid.ColumnWidth = size{2};
             grid.Padding = padding;
+        end
+
+        function closeSubPanels(o)
+            if ~isempty(o.labelPanel); delete(o.labelPanel); o.labelPanel = []; end
+            if ~isempty(o.markerPanel); delete(o.markerPanel); o.markerPanel = []; end
+        end
+
+        function struct = renameField(struct,oldLabel,newLabel)
+            struct.(newLabel) = struct.(oldLabel);
+            struct = rmfield(struct, oldLabel);
+        end
+
+        function changeGeomatplotLabel(Geomatplot,oldLabel,newLabel)
+            if isfield(Geomatplot.movs,oldLabel)
+                Geomatplot.movs = PropertiesPanel.renameField(Geomatplot.movs,oldLabel,newLabel);
+            elseif isfield(Geomatplot.deps,oldLabel)
+                Geomatplot.deps = PropertiesPanel.renameField(Geomatplot.deps,oldLabel,newLabel);
+            end
         end
 
         function setColor(o,~,evt)
@@ -151,31 +212,30 @@ classdef PropertiesPanel < handle
             end
         end
 
-        function changeGeomatplotLabel(Geomatplot,oldLabel,newLabel)
-            if isfield(Geomatplot.movs,oldLabel)
-                Geomatplot.movs = PropertiesPanel.renameField(Geomatplot.movs,oldLabel,newLabel);
-            elseif isfield(Geomatplot.deps,oldLabel)
-                Geomatplot.deps = PropertiesPanel.renameField(Geomatplot.deps,oldLabel,newLabel);
-            end
-        end
-
-        function struct = renameField(struct,oldLabel,newLabel)
-            struct.(newLabel) = struct.(oldLabel);
-            struct = rmfield(struct, oldLabel);
+        function setMarkerSize(o,~,evt)
+            size = floor(evt.Value);
+            markerSizeLabel = o.markerPanel.Children(1).Children(2);
+            markerSizeLabel.Text = string(size);
+            if isa(o.geometry,'point_base'); o.geometry.fig.MarkerSize = size + 2;
+            elseif isa(o.geometry,'dlines'); o.geometry.fig.LineWidth = size + 1; end
         end
 
         function switchLabel(o,~,evt)
             o.geometry.fig.LabelVisible = string(matlab.lang.OnOffSwitchState(evt.Value));
         end
 
-        function closeSubPanels(o)
-            if ~isempty(o.labelPanel); delete(o.labelPanel); o.labelPanel = []; end
-        end
+        function switchLabelPanel(o)
+            if ~isempty(o.markerPanel); delete(o.markerPanel); o.markerPanel = []; end
 
-        function switchLabelPanel(o,~,~)
-            %Should close other sub panels later
             if isempty(o.labelPanel); PropertiesPanel.createLabelPanel(o);
             else; delete(o.labelPanel); o.labelPanel = []; end
+        end
+
+        function switchMarkerPanel(o,size,limits)
+            if ~isempty(o.labelPanel); delete(o.labelPanel); o.labelPanel = []; end
+
+            if isempty(o.markerPanel); PropertiesPanel.createMarkerPanel(o,size,limits);
+            else; delete(o.markerPanel); o.markerPanel = []; end
         end
     end
 end

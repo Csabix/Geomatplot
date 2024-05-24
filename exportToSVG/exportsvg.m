aaf8fdb7271be53a56371764da7fa5ead9888d06
unknown <tobo2002.19@gmail.com>
2024-05-24 12:32:47 +0200

    small fixes

--- exportToSVG/exportsvg.m
+++ exportToSVG/exportsvg.m
@@ -5,13 +5,13 @@ function exportsvg(g, location, dashedEnabled, dottedEnabled, interactive, scale
     if isempty(get(g, 'UserData'))
         fig = get(g,'parent');
         close(fig);
-        error("Error! There are no plots to export!\nHiba! Nincs exportálható adat");
+        error("Error! There are no plots to export! | Hiba! Nincs exportálható adat");
     end
     
     if ~isequal(class(userData), 'Geomatplot')
         fig = get(g,'parent');
         close(fig);
-        error("Error! This plot cannot be exported (not made with Geomatplot)\nHiba! Ez a koordinátarendszer nem Geomatplottal lett létrehozva");
+        error("Error! This plot cannot be exported (not made with Geomatplot) | Hiba! Ez a koordinátarendszer nem Geomatplottal lett létrehozva");
     end
 
     locationDefault = './export.svg';
@@ -51,10 +51,14 @@ function exportsvg(g, location, dashedEnabled, dottedEnabled, interactive, scale
     end
 
     rawData = ExtractUserData(userData);
-    disp(location);
+    %disp(location);
     
-    location = CorrectLocationName(location);
-    outFile = fopen(location,'w');
+    try
+        location = CorrectLocationName(location);
+        outFile = fopen(location,'w');
+    catch e
+        error("Error! Invalid file name\nHiba! Rossz fájl név");
+    end
 
     xWidth = g.XLim(2) - g.XLim(1);
     yWidth = g.YLim(2) - g.YLim(1);
@@ -67,9 +71,10 @@ function exportsvg(g, location, dashedEnabled, dottedEnabled, interactive, scale
 end
 
 function loc = CorrectLocationName(location)
+    loc = location;
     if isfolder(location)
         loc = strcat(location, 'export.svg');
-    elseif numel(location) <= 3
+    elseif numel(location) > 0 && numel(location) <= 3
         loc = strcat(location, '.svg');
     elseif ~isequal(location(numel(location)-3:numel(location)), '.svg')
         loc = strcat(location, '.svg');

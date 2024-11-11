3a825d492e66504718911a30d5615b0342f234ed
Levente Fodor <leventefodor@gmail.com>
2024-11-11 16:45:00 +0100

    Added naming distinctions for sliders.

--- drawSliderX.m
+++ drawSliderX.m
@@ -11,7 +11,7 @@ function [val,Pt,txt] = drawSliderX(varargin)
     
     Pt = Point(parent,label,args.startPos + [args.slideLen*((args.startVal-args.range(1))/diff(args.range)), 0],Seg,'LabelV','off','Visible',args.Visible);
     val = Scalar(parent,Pt, @(dp) (dp(1)-args.startPos(1))/args.slideLen*diff(args.range)+args.range(1));
-    txt = Text(parent,Point(parent,Pt+[-0.16 -0.04],'Visible','off'), val, @(v) Pt.label + " = " + num2str(v),'Visible',args.Visible);
+    txt = Text(parent,Point(parent,"TPos" + label,Pt+[-0.16 -0.04],'Visible','off'), val, @(v) Pt.label + " = " + num2str(v),'Visible',args.Visible);
 end
 
 function params = parse_slider(range,startPos,slideLen,startVal,params)

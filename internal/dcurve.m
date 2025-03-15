d91b1efabe8d1e42f7c081d076f8bf6bb659427d
Csabix <csabix@inf.elte.hu>
2025-03-15 13:13:39 +0100

    Added Curve.fun dependent function for easy curve eval.

--- internal/dcurve.m
+++ internal/dcurve.m
@@ -1,17 +1,26 @@
 classdef dcurve < dlines
     properties
         Resolution (1,1) double = 512;
+        fun;
     end
     methods
         function o = dcurve(parent,label,inputs,callback,params,resolution)
             o = o@dlines(parent,label,inputs,[],params);
             if nargin >= 6; o.Resolution = resolution; end
             o.setUpdateCallback(callback);
+            o.fun = [];
         end
         function update(o,detail_level)
             if nargin < 2; detail_level = 1; end
             t = linspace(0,1,o.Resolution*detail_level)';
             o.call(t);
+        end
+        function f = get.fun(o)
+            if isempty(o.fun)
+                nlabel = o.parent.extractLabel({},'custom');
+                o.fun = dcustomvalue(o.parent,nlabel,{o}, @(o) @(t) o.callback(t,o.inputs{:}) );
+            end
+            f = o.fun;
         end
     end
 end

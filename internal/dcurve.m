0bea0bc8a4013ba3e54a8a01435dfd5d06f2924d
Csabix <csabix@inf.elte.hu>
2025-03-15 14:04:31 +0100

    Added Image.fun dependent function for easy 2D function eval
    Only for real matrix input.

--- internal/dcurve.m
+++ internal/dcurve.m
@@ -1,14 +1,13 @@
 classdef dcurve < dlines
     properties
-        Resolution (1,1) double = 512;
-        fun;
+        Resolution (1,1) double = 512
+        fun = []
     end
     methods
         function o = dcurve(parent,label,inputs,callback,params,resolution)
             o = o@dlines(parent,label,inputs,[],params);
             if nargin >= 6; o.Resolution = resolution; end
             o.setUpdateCallback(callback);
-            o.fun = [];
         end
         function update(o,detail_level)
             if nargin < 2; detail_level = 1; end
